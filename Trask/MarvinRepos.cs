6249eaae3951ee043617024db6b30844cf46dbed
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-28 06:48:52 +0200

    Build fix

--- Trask/MarvinRepos.cs
+++ Trask/MarvinRepos.cs
@@ -1,44 +1,5 @@
 namespace SunamoDevCode;
 
-public class MarvinReposName
-{
-    public const string Marvin = "Marvin";
-    public const string MarvinIaC = "Marvin.IaC";
-    public const string MarvinDeployment = "MarvinDeployment";
-    public const string ModuleAdministration = "Module.Administration";
-    public const string ModuleB2CVCM = "Module.B2CVCM";
-    public const string ModuleAMSTools = "Module.AMSTools";
-    public const string ModuleCookieManagement = "Module.CookieManagement";
-    public const string ModuleDDM = "Module.DDM";
-    public const string ModuleDemo = "Module.Demo";
-    public const string ModuleDipon = "Module.Dipon";
-    //public const string ModuleDemoK2NG = "Module.DemoK2NG";
-    public const string ModuleEMobilityToolsAdministration = "Module.EMobilityToolsAdministration";
-    public const string ModuleEscalationSheet = "Module.EscalationSheet";
-    public const string ModuleFinanceCalculator = "Module.FinanceCalculator";
-    public const string ModuleForms = "Module.Forms";
-    //public const string ModuleExample = "Module.Example";
-    public const string ModuleIASAdministration = "Module.IASAdministration";
-    public const string ModuleImporterSettings = "Module.ImporterSettings";
-    public const string ModuleNewDemo = "Module.NewDemo";
-    public const string ModuleOwnersManual = "Module.OwnersManual";
-    public const string ModulePAudit = "Module.PAudit";
-    public const string ModulePCI = "Module.PCI";
-    public const string Module = "Module.PVP";
-    public const string ModuleRecommendationEngine = "Module.RecommendationEngine";
-    public const string ModuleRedirector = "Module.Redirector";
-    public const string ModuleScoringModel = "Module.ScoringModel";
-    public const string ModuleSRS = "Module.SRS";
-    public const string ModuleStatistics = "Module.Statistics";
-    public const string ModuleTDICUST = "Module.TDI-CUST";
-    public const string ModuleVbto = "Module.VBtO";
-
-    public const string ModuleTraffication = "Module.Traffication";
-    public const string ModuleVisualizer = "Module.Visualizer";
-    public const string ModuleVMAC = "Module.VMAC";
-
-    public const string Shared = "Shared";
-}
 
 public class MarvinRepos
 {
