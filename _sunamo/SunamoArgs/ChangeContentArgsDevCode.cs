aa6dab584934603bc2a469b0f8dfd8ae8766f7aa
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 11:37:47 +0200

    Make some classes public when it is needed

---
+++ _sunamo/SunamoArgs/ChangeContentArgsDevCode.cs
@@ -0,0 +1,9 @@
+namespace SunamoDevCode;
+
+
+internal class ChangeContentArgsDevCode
+{
+    internal bool removeNull = false;
+    internal bool removeEmpty = false;
+    internal bool switchFirstAndSecondArg = false;
+}
---
+++ _sunamo/SunamoData/Data/FromToDevCode.cs
@@ -0,0 +1,35 @@
+namespace SunamoDevCode;
+
+
+/// <summary>
+///     Must have always entered both from and to
+///     None of event could have unlimited time!
+/// </summary>
+internal class FromToDevCode : FromToTSHDevCode<long>
+{
+    internal static FromToDevCode Empty = new(true);
+    internal FromToDevCode()
+    {
+    }
+    /// <summary>
+    ///     Use Empty contstant outside of class
+    /// </summary>
+    /// <param name="empty"></param>
+    private FromToDevCode(bool empty)
+    {
+        this.empty = empty;
+    }
+    /// <summary>
+    ///     A3 true = DateTime
+    ///     A3 False = None
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="ftUse"></param>
+    internal FromToDevCode(long from, long to, FromToUse ftUse = FromToUse.DateTime)
+    {
+        this.from = from;
+        this.to = to;
+        this.ftUse = ftUse;
+    }
+}
