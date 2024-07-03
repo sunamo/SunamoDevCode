5d00ed05ded4e3296567bea2c4240b7909f4c2e9
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-03 07:51:39 +0200

    24.7.3.3 internal to public

--- GlobalUsingsInstance.cs
+++ GlobalUsingsInstance.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-internal class GlobalUsingsInstance
+public class GlobalUsingsInstance
 {
     ParseGlobalUsingsResult r = null;
     string path;
