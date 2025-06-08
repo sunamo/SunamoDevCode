e95779419f5bcee0992a70f8a01836de703448fd
sunamo.cz <radek.jancik@sunamo.cz>
2025-06-08 22:44:35 +0200

    Moved to separate file

--- GetCsprojs.cs
+++ GetCsprojs.cs
@@ -2,7 +2,7 @@ namespace SunamoDevCode;
 
 using Microsoft.Extensions.Logging;
 
-public class GetCsprojs
+public partial class GetCsprojs
 {
 
     /// <summary>
@@ -64,70 +64,10 @@ public class GetCsprojs
         return f;
     }
 
-    /// <summary>
-    /// 1 - csprojFolderPaths
-    /// 2 - csprojPaths
-    /// </summary>
-    /// <param name="slnFolder"></param>
-    /// <returns></returns>
-    public static (List<string>, List<string>) GetCsprojInSolution(ILogger logger, string slnFolder)
-    {
-        List<string> csprojPaths = [];
-
-        var csprojFolderPaths = FSGetFolders.GetFoldersEveryFolderWhichContainsFiles(logger, slnFolder, "*.csproj", SearchOption.TopDirectoryOnly);
-        for (int i = csprojFolderPaths.Count - 1; i >= 0; i--)
-        {
-            var fi = csprojFolderPaths[i];
-            var csprojs = FSGetFiles.GetFilesEveryFolder(logger, fi, "*.csproj", SearchOption.TopDirectoryOnly).ToList();
-
-            if (csprojs.Count == 0)
-            {
-                Error("No csproj in " + fi);
-                csprojFolderPaths.RemoveAt(i);
-            }
-            else if (csprojs.Count > 1)
-            {
-                foreach (var item in csprojs.Where(d => d.Contains(" - Backup")))
-                {
-                    File.Delete(item);
-                }
-                csprojs = csprojs.Where(d => !d.Contains(" - Backup")).ToList();
-                if (csprojs.Count > 1)
-                {
-                    Error("More than one csproj in " + fi);
-                    csprojFolderPaths.RemoveAt(i);
-                }
-            }
-            else
-            {
-                csprojPaths.Add(csprojs[0]);
-            }
-            //E:\vs\Projects\PlatformIndependentNuGetPackages\SunamoCl\SunamoCl.csproj
-        }
-
-        csprojPaths.Reverse();
-
-        return (csprojFolderPaths, csprojPaths);
-    }
-
     static void Error(string s)
     {
         throw new Exception(s);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     public static CsprojsInSolution GetCsprojInSwdNotmineAndSunamo(ILogger logger)
     {
@@ -139,50 +79,6 @@ public class GetCsprojs
 
         //return PlatformIndependentNuGetPackages.Intersect(sunamoNotmine);
     }
-
-    public static CsprojsInSolution GetCsprojInSolutionClass(ILogger logger, string slnFolder, bool automaticallyAddToUniqueCsprojs = true)
-    {
-        var (csprojNames, csprojPaths) = GetCsprojInSolution(logger
-            , slnFolder);
-        var result = new CsprojsInSolution { CsprojFolderPaths = csprojNames, CsprojPaths = csprojPaths };
-        if (automaticallyAddToUniqueCsprojs)
-        {
-            UniqueCsprojs.AddFromSlnFolder(result);
-        }
-
-        return result;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public static List<string> GetCsprojsInSolution(ILogger logger, string slnFolder)
-    {
-        var f = GetFoldersWithAtLeastOneCsprojInSolution(logger, slnFolder);
-        List<string> result = new List<string>(f.Count);
-        foreach (var item in f)
-        {
-            result.Add(FSGetFiles.GetFilesEveryFolder(logger, item, "*.csproj", SearchOption.TopDirectoryOnly)[0]);
-        }
-        return result;
-    }
-
-
-
-    //private static void Error(string v)
-    //{
-    //    throw new Exception(v);
-    //}
 
     public static Dictionary<string, string> GetCsprojsAllDict(ILogger logger)
     {
